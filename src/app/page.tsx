b21c993126e3dffb1900b837babc11a14f73113a
Lucca Santos <lucasluccad.r@outlook.com>
2023-05-19 11:18:38 -0300

    feat: add stars as background in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <main className="grid min-h-screen w-screen grid-cols-2">
+    <main className="grid min-h-screen w-screen grid-cols-2 bg-[url(../assets/bg-stars.svg)] bg-cover">
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden  border-r border-white/10 px-10 py-6"></div>
 
       {/* Right */}
-      <div className="flex flex-col p-16">
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
             Você ainda não criou nenhuma lembrança, comece a{' '}
