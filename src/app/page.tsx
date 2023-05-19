cc54c5fcdf4939f63a655dcada7137ee59e8cd4e
Lucca Santos <lucasluccad.r@outlook.com>
2023-05-19 11:06:04 -0300

    feat: add home page initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
-    <main>
-      <h1>NLW Spacetime</h1>
+    <main className="grid min-h-screen w-screen grid-cols-2">
+      {/* Left */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden  border-r border-white/10 px-10 py-6"></div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não criou nenhuma lembrança, comece a{' '}
+            <a href="#" className="hover:text-gray-50">
+              criar agora
+            </a>{' '}
+          </p>
+        </div>
+      </div>
     </main>
   )
 }
