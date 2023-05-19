9bdabafb4b7b9d579f49a9580f651b20f8b315b9
Lucca Santos <lucasluccad.r@outlook.com>
2023-05-19 20:15:12 -0300

    feat: add route to get code from github and send to api to return token

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,9 +1,11 @@
 import { User } from 'lucide-react'
 
 export function SignIn() {
+  const CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID
+
   return (
     <a
-      href="#"
+      href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`}
       className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
     >
       <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
