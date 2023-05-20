689fe54e65a1f1bace40b298f057e411c075d2c1
Lucca Santos <lucasluccad.r@outlook.com>
2023-05-19 22:06:14 -0300

    feat: show Profile component when authenticated user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
+import { cookies } from 'next/headers'
 import { Blur } from '@/components/Blur'
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { SignIn } from '@/components/SignIn'
 import { Stripes } from '@/components/Stripes'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen w-screen grid-cols-2 bg-[url(../assets/bg-stars.svg)] bg-cover">
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden  border-r border-white/10 px-10 py-6">
         <Blur />
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
         <Stripes />
